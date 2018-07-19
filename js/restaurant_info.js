85dc1b267f9434b37dfd59fb84afca3b859f43e4
Brandon Lothaire Atwell <batwell31@yahoo.com>
2018-07-19 08:09:34 -0400

    add 'MapBox API key' to the mapboxToken as a value

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiYmF0d2VsbDMxIiwiYSI6ImNqanE3ZGdpYjdva2YzcHI1ZHQ1c3Z4bzAifQ.bGMwlE6zU9ZVCmdH4VTRcg',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
