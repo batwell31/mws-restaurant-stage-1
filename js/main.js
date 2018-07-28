16d602d51440fcfb70737e762a755f34639dcd08
Brandon <37312506+batwell31@users.noreply.github.com>
2018-07-28 15:33:32 -0400

    change restaurant name to h2 from h1

--- js/main.js
+++ js/main.js
@@ -181,7 +181,7 @@ createRestaurantHTML = (restaurant) => {
   li.append(image);
 
   // Creates the 'Name' Section
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
